26030d79bf162b0d78d982e20fccdaf944275e73
White Rose <85945510+paixaoalmeida@users.noreply.github.com>
2023-05-03 06:27:19 -0300

    Update Creatine.py

--- Creatine.py
+++ Creatine.py
@@ -1,11 +1,13 @@
 #Código iniciado no dia 1 de maio
+import time
 
 import keys #Importando as informações de login do Twilio de um arquivo no mesmo repositório
 import os
 from requests_html import HTMLSession
 from twilio.rest import Client
-
-def SendMessage(): #Função send_message com a API do Twilio
+from datetime import datetime
+from time import sleep
+def SendMessage(): #Função send_email com a API do Twilio
     client = Client(keys.account_sid, keys.auth_token)
 
     message = client.messages \
@@ -17,27 +19,32 @@ def SendMessage(): #Função send_message com a API do Twilio
     )
 
     print(message.sid)
-    
-def CheckCreatine():
-    global text_list #Definindo uma váriavel global pra eu poder usar ela fora de função (vai virar uma array)
 
-    session = HTMLSession()  # Objeto com a função da lib
-    r = session.get('https://www.gsuplementos.com.br/creatina-250g-creapure-growth-supplements-p985824')
+big_loop = True
+while big_loop:
+    def CheckCreatine():
+        global text_list #Definindo uma váriavel global pra eu poder usar ela fora de função (vai virar uma array)
 
-    r.html.render(sleep=3)  # Renderiza o JavaScript da página
+        session = HTMLSession()  # Objeto com a função da lib
+        r = session.get('https://www.gsuplementos.com.br/creatina-250g-creapure-growth-supplements-p985824')
 
-    comp = r.html.find('.botao-de-compra > div:nth-child(1) > a:nth-child(1) > button:nth-child(1)',first=True)  # A primeira que achar, vai te dar, caso contrário, vai dar uma lista e vai dar erro
+        r.html.render(sleep=3)  # Renderiza o JavaScript da página
 
-    text = comp.text  # Retorna o texto do elemento que vc marcou
-    text_list = [text] #Coloca o resultado do texto numa lista
+        comp = r.html.find('.botao-de-compra > div:nth-child(1) > a:nth-child(1) > button:nth-child(1)',first=True)  # A primeira que achar, vai te dar, caso contrário, vai dar uma lista e vai dar erro
+
+        text = comp.text  # Retorna o texto do elemento que vc marcou
+        text_list = [text] #Coloca o resultado do texto numa lista
 
 
-CheckCreatine() #Roda a função para verificar a disponibilidade
+    CheckCreatine() #Roda a função para verificar a disponibilidade
 
-
-if 'AVISE-ME QUANDO CHEGAR' in text_list: #Se um dos dados parseados estiver dessa maneira, não tem disponível
-    print("TÁ SEM!")
-    exit(0)
-elif 'AVISE-ME QUANDO CHEGAR' not in text_list: #Caso contrário, aparece 'comprar', então pode enviar o e-mail
-    SendMessage() #Função que envia o sms
-
+    loop = True #Variável para usar como uma flag no loop
+    while loop:
+        if 'AVISE-ME QUANDO CHEGAR' in text_list: #Se um dos dados parseados estiver dessa maneira, não tem disponível
+            print("NÃO DISPONÍVEL! " + str(datetime.now())) #Printa o horário para fins de registro
+            time.sleep(120) #Como é um código para monitoramento, se o produto não estiver disponível, aguarda um pouco e volta tudo novamente
+            break #Após os 2 minutos de espera, volta para o loop anterior e reinicia o ciclo
+        elif 'AVISE-ME QUANDO CHEGAR' not in text_list: #Caso contrário, aparece 'comprar', então pode enviar o e-mail
+            SendMessage() #Função que envia o sms
+            big_loop = False
+            exit(0)
