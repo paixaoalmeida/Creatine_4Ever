d9171d5412ca3c57a9241fa90255e3759ccae8c2
paixaoalmeida <paixaowork5@gmail.com>
2023-07-16 16:49:59 -0300

    Not needed anymore since we will not use twillio

--- keys.py
+++
@@ -1,9 +0,0 @@
-account_sid = 'YOUR ID'
-auth_token = 'YOUR_TOKEN'
-
-twilio_number = 'YOUR_TWILLIO_NUMBER'
-target_number = 'TARGET_NUMBER'
-
-#Se você não encodar essas credenciais, o programa irá dar erro (não é um erro muito intuitivo, então será difícil de você saber o que é
-
-#Só consegue ver o que era depois de olhar a fundo a biblioteca que estava dando erro
