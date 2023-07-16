bf3db534851786e27d3a9c40501426461391c6be
paixaoalmeida <paixaowork5@gmail.com>
2023-07-16 11:16:48 -0300

    New changes

--- Creatine.py
+++ Creatine.py
@@ -1,60 +1,79 @@
-#Código iniciado no dia 1 de maio
-#LIBRARIES------------------------------------------LIBRARIES-------------------------------LIBRARIES
 import time
-import Selenium_buy as Robot_buy #importando o arquivo do selenium que realiza a compra
-import keys #Importando as informações de login do Twilio de um arquivo no mesmo repositório
 import os
-from requests_html import HTMLSession
-from twilio.rest import Client
-from datetime import datetime
 from time import sleep
+from datetime import datetime
 
-#FUNÇÕES---------------------------------------FUNÇÕES------------------------------------------FUNÇÕES
-def SendMessage(): #Função send_email com a API do Twilio
-    client = Client(keys.account_sid, keys.auth_token)
+import pywhatkit
+from requests_html import HTMLSession
 
-    message = client.messages \
-        .create(
-        body='Creatina Creapure está disponível! Estamos comprando pra você, se não deu certo, compre manualmente no link: https://www.gsuplementos.com.br/creatina-250g-creapure-growth-supplements-p985824',
-        from_=keys.twilio_number,
-        status_callback='https://abc1234.free.beeceptor.com',
-        to=keys.target_number
-    )
 
-    print(message.sid)
+def send_message():
+    """Send message via WhatsApp
 
-def CheckCreatine():
-        global text_list #Definindo uma váriavel global pra eu poder usar ela fora de função (vai virar uma array)
+    This function uses the pywhatkit lib to
+    send messages via WhatsApp to the sender
 
-        session = HTMLSession()  # Objeto com a função da lib
+    Here, in order to be able to fit the requirements
+    of pywhatkit, we have to put the hour and minute as integers
+    with no zero before, so i converted these in the variables now_hour
+    and now_min
+
+    Since it's also recommended by the library to wait a little before
+    sendind the message, we add 3 minutes more to send the message"""
+
+    link_creatine = 'https://www.gsuplementos.com.br/creatina-250g-creapure-growth-supplements-p985824'
+
+    now_hour = datetime.strptime(str(datetime.now()), '%Y-%m-%d %H:%M:%S.%f').strftime('%-H')
+    now_min = datetime.strptime(str(datetime.now()), '%Y-%m-%d %H:%M:%S.%f').strftime('%-M')
+
+    pywhatkit.sendwhatmsg('+5511984218991', f'Creatina disponivel!\n {link_creatine}', int(now_hour), int(now_min) + 3)
+
+
+def check_creatine() -> list:
+        """Aqui abrimos a página e carregamos o javascript da página
+        com a funcao html.find nós monitoramos o botão de compra da creatina
+        
+        Retornamos a array text_list para a função abaixo usar ela na
+        verificação"""
+        session = HTMLSession()
         r = session.get('https://www.gsuplementos.com.br/creatina-250g-creapure-growth-supplements-p985824')
 
-        r.html.render(sleep=5)  # Renderiza o JavaScript da página
 
-        comp = r.html.find('.botao-de-compra > div:nth-child(1) > a:nth-child(1) > button:nth-child(1)',first=True)  # A primeira que achar, vai te dar, caso contrário, vai dar uma lista e vai dar erro
-
-        text = comp.text  # Retorna o texto do elemento que vc marcou
-        text_list = [text] #Coloca o resultado do texto numa lista
-
-def BuyS():
-    print("Efetuando a compra da Creatina em " + str(datetime) + "....")
-    Robot_buy.BuyCreatine
+        r.html.render(sleep=5)          #Rendering the JavaScript page
 
 
-#CÓDIGO--------------------------------------CÓDIGO-------------------------------------CÓDIGO------
-if __name__ == "__main__":
-    big_loop = True
-    while big_loop:
-        CheckCreatine() #Roda a função para verificar a disponibilidade
+        comp = r.html.find('.botao-de-compra > div:nth-child(1) > a:nth-child(1) > button:nth-child(1)',first=True)
+        #A primeira que achar, vai te dar, caso contrário, vai dar uma lista e vai dar erro
 
 
-        loop = True #Variável para usar como uma flag no loop
-        while loop:
-            if 'AVISE-ME QUANDO CHEGAR' in text_list: #Se um dos dados parseados estiver dessa maneira, não tem disponível
-                print("NÃO DISPONÍVEL! " + str(datetime.now())) #Printa o horário para fins de registro
-                time.sleep(120) #Como é um código para monitoramento, se o produto não estiver disponível, aguarda um pouco e volta tudo novamente
-                CheckCreatine() #Volta a função para verificar a disponibilidade
-            elif 'AVISE-ME QUANDO CHEGAR' not in text_list: #Caso contrário, aparece 'comprar', então pode enviar o e-mail
-                SendMessage() and BuyS #Função que envia o sms
-                big_loop = False
-                exit(0)
+        text = comp.text
+        text_list: list = [text]            #Var containing the response of the scrapping at the button
+
+        return text_list
+
+
+def do_stuff(text_list:list) -> None:
+    """Here we just do some simple if-else
+    verification
+    
+    If the button is not with the AVISE ME
+    QUANDO CHEGAR, then its available
+    """
+    while True:
+        if 'AVISE-ME QUANDO CHEGAR' in text_list: 
+            print("NÃO DISPONÍVEL! " + datetime.strptime(str(datetime.now()),'%Y-%m-%d %H:%M:%S.%f').strftime('%d/%m %H:%M'))
+            time.sleep(120) 
+        elif 'AVISE-ME QUANDO CHEGAR' not in text_list: 
+            send_message()
+            exit()
+
+
+def main():
+    """Main function of the script
+
+    All functions of the programm are run here"""
+    text_list = check_creatine()
+    do_stuff(text_list)
+
+
+main()
